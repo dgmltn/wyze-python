fe85ce9e4d4decb70680a87aa13c166a9207ebd4
Doug Melton <what@supbro.com>
2023-03-16 18:01:43 +0000

    Code cleanup

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
+# flask
 from flask import Flask, render_template, redirect, url_for, request
-from dotenv import load_dotenv
+
+# wyze
 from wyze_sdk import Client
 from wyze_sdk.errors import WyzeApiError
 
+# crypto
 from hashlib import md5
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 from base64 import b64decode
 from base64 import b64encode
 
+# system utils
 import json
 import requests
 import os
@@ -16,9 +20,8 @@ import sys
 import re
 import time
 
-load_dotenv()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("CSRF_SECRET_KEY")
+app.config['SECRET_KEY'] = os.urandom(12).hex()
 
 cached_client_expiration = 0
 cached_client_ttl = 60 * 60 * 12
@@ -47,59 +50,63 @@ def get_client():
         cached_client_expiration = now + cached_client_ttl
     return cached_client
 
-def send_local_post(ip, mac, enr):
-    characteristics = """
-{{
-    "mac": "{mac}",
-    "index": "0",
-    "ts": "{ts}",
-    "plist": [
-        {{
-            "pid": "P3",
-            "pvalue": "1"
-        }},
-        {{
-            "pid": "P1507",
-            "pvalue": "ff00ff"
-        }},
-        {{
-            "pid": "P1501",
-            "pvalue": "100"
-        }}
-    ]
-}}""".format(mac=mac, ts=int(time.time() * 1000))
+
+def api_bulb_set(bulb, onoff="", color="", brightness="", temp=""):
+    client = get_client()
+    if onoff == "on":
+        client.bulbs.turn_on(device_mac=bulb.mac, device_model=bulb.product.model)
+    elif onoff == "off":
+        client.bulbs.turn_off(device_mac=bulb.mac, device_model=bulb.product.model)
+
+    if color:
+        client.bulbs.set_color(device_mac=bulb.mac, device_model=bulb.product.model, color=color)
+    elif temp:
+        client.bulbs.set_color_temp(device_mac=bulb.mac, device_model=bulb.product.model, color_temp=temp)
+
+    if brightness:
+        client.bulbs.set_brightness(device_mac=bulb.mac, device_model=bulb.product.model, brightness=brightness)
+
+
+def local_bulb_set(bulb, onoff="", color="", brightness="", temp=""):
+    characteristics = {
+        "mac": bulb.mac,
+        "index": "0",
+        "ts": str(int(time.time() * 1000)),
+        "plist": []
+    }
 
     # P3 - on/off (1 = on, 0 = off)
     # P1501 - brightness (0 - 100)
     # P1502 - temperature (1000 - 6000)
     # P1507 - rgb (00ff00)
 
-    encrypted = AESCipher(enr, enr).encrypt(characteristics)
-    #decrypted = AESCipher(enr, enr).decrypt(encrypted)
+    if onoff == "on":
+        characteristics["plist"].append({ "pid": "P3", "pvalue": "1" })
+    elif onoff == "off":
+        characteristics["plist"].append({ "pid": "P3", "pvalue": "0" })
+
+    if color:
+        characteristics["plist"].append({ "pid": "P1507", "pvalue": color })
+    elif temp:
+        characteristics["plist"].append({ "pid": "P1502", "pvalue": str(temp) })
+
+    if brightness:
+        characteristics["plist"].append({ "pid": "P1501", "pvalue": str(brightness) })
+
+    if not len(characteristics["plist"]):
+        return
+
+    dumped = json.dumps(characteristics, separators=(',',':'))
+    encrypted = AESCipher(bulb.enr, bulb.enr).encrypt(dumped)
+    #decrypted = AESCipher(bulb.enr, bulb.enr).decrypt(encrypted)
 
     body = {
         "request": "set_status",
         "isSendQueue": 0,
         "characteristics": encrypted
     }
-    bodyString = json.dumps(body,separators=(',', ':'))
-    print(f"bodyString: {bodyString}")
-    headers = {
-        "Content-Type": "application/json",
-        "Content-Length": str(len(bodyString)),
-    }
-    response = requests.post(f'http://{ip}:88/device_request', headers=headers, data=bodyString)
+    response = requests.post(f'http://{bulb.ip}:88/device_request', json=body)
     print(response.__dict__, file=sys.stderr)
-
-@app.route('/testlocal')
-def localtest():
-    # console light:
-    #send_local_post("10.5.27.4", "7C78B2212876", "QvoMI4VyzSn7oXDw")
-    # bedside bulb:
-    send_local_post("10.5.27.7", "7C78B21DFC9F", "bxJs6+NdUP9KOv9h")
-
-    return "ok", 200
-
 
 @app.route('/')
 def get_lights():
@@ -127,85 +134,89 @@ def bulb_get(mac):
         return render_template('error.html', error=e)
 
 
-@app.route('/bulb/set/<mac>/<model>', methods = ['GET', 'POST'])
-def bulb_set(mac, model):
-    client = get_client()
-    try:
-        if 'onoff' in request.values and request.values['onoff']:
-            onoff = request.values['onoff']
-            if onoff == 'on':
-                client.bulbs.turn_on(device_mac=mac, device_model=model)
-            elif onoff == 'off':
-                client.bulbs.turn_off(device_mac=mac, device_model=model)
+@app.route('/bulb/set/<mac>', methods = ['GET', 'POST'])
+def bulb_set(mac):
 
-        if 'brightness' in request.values and request.values['brightness']:
-            brightness = request.values['brightness']
-            client.bulbs.set_brightness(device_mac=mac, device_model=model, brightness=brightness)
+    onoff = ""
+    brightness = ""
+    temp = ""
+    color = ""
 
-        if 'temp' in request.values and request.values['temp']:
-            temp = request.values['temp']
-            client.bulbs.set_color_temp(device_mac=mac, device_model=model, color_temp=temp)
+    if 'onoff' in request.values and request.values['onoff']:
+        onoff = request.values['onoff']
 
-        elif 'color' in request.values and request.values['color']:
-            color = request.values['color']
-            client.bulbs.set_color(device_mac=mac, device_model=model, color=color)
+    if 'brightness' in request.values and request.values['brightness']:
+        brightness = request.values['brightness']
 
-        if 'q' in request.values and request.values['q']:
-            q = request.values['q']
-            print(f'matching q = "{q}"', file=sys.stderr)
+    if 'temp' in request.values and request.values['temp']:
+        temp = request.values['temp']
 
-            m = re.search(r'\bon\b', q)
-            if (m):
-                print('matches for on', file=sys.stderr)
-                client.bulbs.turn_on(device_mac=mac, device_model=model)
+    elif 'color' in request.values and request.values['color']:
+        color = request.values['color']
 
-            m = re.search(r'\boff\b', q)
-            if (m):
-                print('matches for off', file=sys.stderr)
-                client.bulbs.turn_off(device_mac=mac, device_model=model)
+    if 'q' in request.values and request.values['q']:
+        q = request.values['q']
+        print(f'matching q = "{q}"', file=sys.stderr)
 
-            m = re.search(r'\bcool|cold\b', q)
-            if (m):
-                print('matches for cool/cold', file=sys.stderr)
-                client.bulbs.set_color_temp(device_mac=mac, device_model=model, color_temp='4300')
+        m = re.search(r'\bon\b', q)
+        if (m):
+            print('matches for on', file=sys.stderr)
+            onoff = "on"
 
-            m = re.search(r'\bdaylight\b', q)
-            if (m):
-                print('matches for daylight', file=sys.stderr)
-                client.bulbs.set_color_temp(device_mac=mac, device_model=model, color_temp='6500')
+        m = re.search(r'\boff\b', q)
+        if (m):
+            print('matches for off', file=sys.stderr)
+            onoff = "off"
 
-            m = re.search(r'\bwarm\b', q)
-            if (m):
-                print('matches for warm', file=sys.stderr)
-                client.bulbs.set_color_temp(device_mac=mac, device_model=model, color_temp='3000')
+        m = re.search(r'\bcool|cold\b', q)
+        if (m):
+            print('matches for cool/cold', file=sys.stderr)
+            temp = "4300"
 
-            m = re.search(r'\b#?([a-fA-F0-9]{6})\b', q)
-            if (m):
-                print(f'matches for rrggbb: #{m[1]}', file=sys.stderr)
-                client.bulbs.set_color(device_mac=mac, device_model=model, color=m[1])
+        m = re.search(r'\bdaylight\b', q)
+        if (m):
+            print('matches for daylight', file=sys.stderr)
+            temp = "6500"
 
-            m = re.search(r'\b(\d{1,3}),(\d{1,3}),(\d{1,3})\b', q)
-            if (m):
-                rgb = '{:02x}{:02x}{:02x}'.format(int(m[1]), int(m[2]), int(m[3]))
-                print(f'matches for rr,gg,bb: {m[1]}, {m[2]}, {m[3]} => #{rgb}', file=sys.stderr)
-                client.bulbs.set_color(device_mac=mac, device_model=model, color=rgb)
+        m = re.search(r'\bwarm\b', q)
+        if (m):
+            print('matches for warm', file=sys.stderr)
+            temp = "3000"
 
-            m = re.search(r'\b(100|\d?\d)%', q)
-            if (m):
-                print(f'matches for brightness: {m[1]}%', file=sys.stderr)
-                client.bulbs.set_brightness(device_mac=mac, device_model=model, brightness=m[1])
+        m = re.search(r'\b#?([a-fA-F0-9]{6})\b', q)
+        if (m):
+            print(f'matches for rrggbb: #{m[1]}', file=sys.stderr)
+            color = m[1]
 
-            m = re.search(r'\b(\d{4})[kK]\b', q)
-            print(m, file=sys.stderr)
-            if (m):
-                print(f'matches for color temperature: {m[1]}K', file=sys.stderr)
-                client.bulbs.set_color_temp(device_mac=mac, device_model=model, color_temp=m[1])
+        m = re.search(r'\b(\d{1,3}),(\d{1,3}),(\d{1,3})\b', q)
+        if (m):
+            rgb = '{:02x}{:02x}{:02x}'.format(int(m[1]), int(m[2]), int(m[3]))
+            print(f'matches for rr,gg,bb: {m[1]}, {m[2]}, {m[3]} => #{rgb}', file=sys.stderr)
+            color = rgb
 
-        return "ok", 200
-        #return redirect(url_for("bulb_get", mac=mac))
+        m = re.search(r'\b(100|\d?\d)%', q)
+        if (m):
+            print(f'matches for brightness: {m[1]}%', file=sys.stderr)
+            brightness = m[1]
 
-    except WyzeApiError as e:
-        return render_template('error.html', error=e)
+        m = re.search(r'\b(\d{4})[kK]\b', q)
+        print(m, file=sys.stderr)
+        if (m):
+            print(f'matches for color temperature: {m[1]}K', file=sys.stderr)
+            temp = m[1]
+
+    if onoff or brightness or color or temp:
+        client = get_client()
+        bulb = client.bulbs.info(device_mac=mac)
+        local_bulb_set(bulb, onoff=onoff, brightness=brightness, color=color, temp=temp)
+        #try:
+            #api_bulb_set(bulb, onoff=onoff, brightness=brightness, color=color, temp=temp)
+        #except WyzeApiError as e:
+            #return render_template('error.html', error=e)
+
+    return "ok", 200
+    #return redirect(url_for("bulb_get", mac=mac))
+
 
 
 if __name__ == '__main__':
